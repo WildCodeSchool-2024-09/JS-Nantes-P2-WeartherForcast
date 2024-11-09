f65eb41018194bda492ba85683d65b937597f800
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-09 19:20:56 +0100

    logo added on header + display flex on header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import "/src/style/header.css";
 import menuBurger from "/src/assets/icons/menu-burger.png";
+import logo from "/src/assets/images/logo.svg";
 
 export default function Header() {
   return (
     <header>
-      <button>
-      <img src={menuBurger} alt="logo Weather Forcast" />
+      <button type="button">
+        <img src={menuBurger} alt="" />
       </button>
       <nav>
         <Link to="/">Geolocalise me</Link>
         <Link to="/myCities">My cities</Link>
         <Link to="/preferenciesWear">My preferencies</Link>
-      </nav>      
+      </nav>
+      <img className="logo" src={logo} alt="logo Weather Forcast" />
     </header>
   );
 }
