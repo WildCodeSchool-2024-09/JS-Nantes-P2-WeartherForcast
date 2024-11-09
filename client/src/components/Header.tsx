1ee780c0eb0c8cb67a134a0cbdfddd7f6578d928
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-09 20:29:03 +0100

    menuHeaderModal in place when it is open

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,36 +1,30 @@
 import "/src/style/header.css";
-import menuBurger from "/src/assets/icons/menu-burger.png";
 import menuBurgerOpen from "/src/assets/icons/burgerMenuOpen.png";
+import menuBurger from "/src/assets/icons/menu-burger.png";
 import logo from "/src/assets/images/logo.svg";
-import { useState } from "react";
-import MenuHeader from "./MenuHeader";
 
-export default function Header() {
+interface HeaderProps {
+  setMenuOpen: (value: boolean) => void;
+  isMenuOpen: boolean;
+}
 
-  //MODAL MENU OPEN STATE
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
+export default function Header({ setMenuOpen, isMenuOpen }: HeaderProps) {
   //ONCLICK BUTTON MENU BURGER
   const handleClick = () => {
-    setMenuOpen(
-      (currentMenuOpen) => !currentMenuOpen
-    ); /* 🆕 A chaque fois que je clique sur mon bouton, je dis que je veux setter la valeur de menu open a l'inverse de sa valeur courante grace a !currentMenuOpen. La fonction dans le set State prend en
-    parametre la valeur courante (currentMenuOpen -> donnee fournit par useState) au moment de mon setState. Cette facon de faire est la facon LAZY qui permet de setter le state de facon decaler 
-    (on lui dot d'attendre d'avoir terminer sa derniere operation),evitant des conflits si un utilisateur fait plein de clic tres vite */
-  }
+    setMenuOpen(isMenuOpen ? false : true); 
+  };
 
   return (
     <header>
       <section className="navBar">
-      <button type="button" onClick={handleClick}>
-        <img src={isMenuOpen ? `${menuBurgerOpen}` : `${menuBurger}`} alt="" />
-      </button>
-      <img className="logo" src={logo} alt="logo Weather Forcast" />
+        <button type="button" onClick={handleClick}>
+          <img
+            src={isMenuOpen ? `${menuBurgerOpen}` : `${menuBurger}`}
+            alt=""
+          />
+        </button>
+        <img className="logo" src={logo} alt="logo Weather Forcast" />
       </section>
-      {isMenuOpen? (
-        <MenuHeader />
-      ) : null}
-     
     </header>
   );
 }
