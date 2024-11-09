c0ff16cb7de93a14b948e5f4ccd6e50c80bcdb75
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-09 17:59:14 +0100

    style -> menuBurger added on header + general button style done in App.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import "/src/style/header.css";
+import menuBurger from "/src/assets/icons/menu-burger.png";
 
 export default function Header() {
   return (
     <header>
+      <button>
+      <img src={menuBurger} alt="logo Weather Forcast" />
+      </button>
       <nav>
         <Link to="/">Geolocalise me</Link>
         <Link to="/myCities">My cities</Link>
         <Link to="/preferenciesWear">My preferencies</Link>
-      </nav>
+      </nav>      
     </header>
   );
 }
