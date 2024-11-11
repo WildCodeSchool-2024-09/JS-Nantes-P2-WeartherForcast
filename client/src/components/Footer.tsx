ff1587deab4f9e1cac67a0019a3e1214046efe6e
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-11 11:34:36 +0100

    style -> footer style done + heartPurple added + <a> style and <a>hover style completed in app.css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
 import "/src/style/footer.css";
+import heartPurple from "/src/assets/icons/heart-purple.png";
 
 export default function Footer() {
   return (
     <footer>
-      <NavLink to="/">Today</NavLink>
-      <NavLink to="/dayByDay">Day By Day</NavLink>
-      <NavLink to="/WeatherNews">Weather News</NavLink>
+      <nav>
+        <NavLink to="/">Today</NavLink>
+        <NavLink to="/dayByDay">Day By Day</NavLink>
+        <NavLink to="/WeatherNews">News</NavLink>
+      </nav>
+      <div>
+        <p>2024 copyright</p>
+        <img src={heartPurple} alt="" />
+        <p>teamUs</p>
+      </div>
     </footer>
   );
 }
