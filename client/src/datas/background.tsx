aa17a9fbc6ef71fa89a4b1261006a501ac716324
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-11 17:23:05 +0100

    backgroundData file added + backgrounds picture added

---
+++ client/src/datas/background.tsx
@@ -0,0 +1,37 @@
+const backgroundData = [
+  {
+    id: 1,
+    url: "/src/assets/images/clouds.jpg",
+    description: "Clouds",
+  },
+  {
+    id: 2,
+    url: "/src/assets/images/rain.jpg",
+    description: "Rain",
+  },
+  {
+    id: 3,
+    url: "/src/assets/images/drizzle.jpg",
+    description: "Drizzle, Mist, Haze",
+  },
+  {
+    id: 4,
+    url: "/src/assets/images/snow.jpg",
+    description: "Snow",
+  },
+  {
+    id: 5,
+    url: "/src/assets/images/clear.jpg",
+    description: "Clear",
+  },
+  {
+    id: 6,
+    url: "/src/assets/images/atmosphere.jpg",
+    description: "Atmosphere",
+  },
+  {
+    id: 7,
+    url: "/src/assets/images/thunderstorm.jpg",
+    description: "Thunderstorm",
+  },
+];
