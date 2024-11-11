bd9c8dddbdfa1d89480cb362724f81737298d048
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-11 10:35:53 +0100

    fonctionnal class active created in app.css + <Link> replace by <NavLink> in order to make links active automatically

--- client/src/components/MenuHeader.tsx
+++ client/src/components/MenuHeader.tsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 export default function MenuHeader() {
   return (
     <section className="menuDropdown">
       <nav>
-        <Link to="/">Geolocalise me</Link>
-        <Link to="/myCities">My cities</Link>
-        <Link to="/preferenciesWear">My preferencies</Link>
+        <NavLink to="/">Geolocalise me</NavLink>
+        <NavLink to="/myCities">My cities</NavLink>
+        <NavLink to="/preferenciesWear">My preferencies</NavLink>
+        {/* 💡 <NavLink/> instead of <Link> allow to give an automatically active className for each link if you do a class named .active DOC=> https://reactrouter.com/en/main/components/nav-link*/}
       </nav>
     </section>
   );
