9b0602dc057247098fd8fcbee60ddf7c28897073
Julien-44100 <julienbotar44100@yahoo.com>
2024-11-15 15:59:24 +0100

    commit avant de fetcher une API meteo

--- client/src/pages/DayByDay.tsx
+++ client/src/pages/DayByDay.tsx
@@ -2,8 +2,11 @@ import "/src/style/DayByDay.css";
 function DayByday() {
   return (
     <section className="day">
-      <div className="rectangle">
-        <div className="small-rectangle" />
+      <div className=".rectangle-parent">
+        <div className="rectangle">
+          <p>M</p>
+          <div className="small-rectangle" />
+        </div>
       </div>
       <div className="rectangle">
         <div className="small-rectangle" />
