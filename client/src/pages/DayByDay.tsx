9c5d83cf927d54641bc1c0007e662320285b3dc3
Julien-44100 <julienbotar44100@yahoo.com>
2024-11-22 11:22:57 +0100

    modification de quelque div de day-text par day-number et changement en css de .day-test par .day-number

--- client/src/pages/DayByDay.tsx
+++ client/src/pages/DayByDay.tsx
@@ -6,33 +6,33 @@ function DayByday() {
       <div className="container-rectangle">
         <div className="rectangle">
           <p className="dbd-text">M</p>
-          <p className="day-text">6</p>
+          <p className="day-number">6</p>
           <img src="../src/assets/images/Cloudy.png" alt="cloud" />
           <div className="small-rectangle" />
         </div>
       </div>
       <div className="rectangle">
         <p className="dbd-text">T</p>
-        <p className="day-text">7</p>
+        <p className="day-number">7</p>
         <img src="../src/assets/images/Cloudy.png" alt="cloud" />
         <div className="small-rectangle" />
       </div>
 
       <div className="rectangle">
         <p className="dbd-text">W</p>
-        <p className="day-text">8</p>
+        <p className="day-number">8</p>
         <img src="../src/assets/images/raining.png" alt="rain" />
         <div className="small-rectangle" />
       </div>
       <div className="rectangle">
         <p className="dbd-text">T</p>
-        <p className="day-text">9</p>
+        <p className="day-number">9</p>
         <img src="../src/assets/images/Blowing-snow.png" alt="snow" />
         <div className="small-rectangle" />
       </div>
       <div className="rectangle">
         <p className="dbd-text">F</p>
-        <p className="day-text">10</p>
+        <p className="day-number">10</p>
         <img src="../src/assets/images/Blowing-snow.png" alt="snow" />
         <div className="small-rectangle" />
       </div>
