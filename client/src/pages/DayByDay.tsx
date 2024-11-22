70ea3982855ac97fd50d2e82f8a2dd608e0ac560
Julien-44100 <julienbotar44100@yahoo.com>
2024-11-22 11:38:54 +0100

    ajout de la div bdb-temperature pour la temperature du matin et soir seulement dans le html

--- client/src/pages/DayByDay.tsx
+++ client/src/pages/DayByDay.tsx
@@ -8,33 +8,43 @@ function DayByday() {
           <p className="dbd-text">M</p>
           <p className="day-number">6</p>
           <img src="../src/assets/images/Cloudy.png" alt="cloud" />
+          <p className="dbd-temperature">17°</p>
           <div className="small-rectangle" />
+          <p className="dbd-temperature">9°</p>
         </div>
       </div>
       <div className="rectangle">
         <p className="dbd-text">T</p>
         <p className="day-number">7</p>
         <img src="../src/assets/images/Cloudy.png" alt="cloud" />
+        <p className="dbd-temperature">18°</p>
         <div className="small-rectangle" />
+        <p className="dbd-temperature">9°</p>
       </div>
 
       <div className="rectangle">
         <p className="dbd-text">W</p>
         <p className="day-number">8</p>
         <img src="../src/assets/images/raining.png" alt="rain" />
+        <p className="dbd-temperature">15°</p>
         <div className="small-rectangle" />
+        <p className="dbd-temperature">7°</p>
       </div>
       <div className="rectangle">
         <p className="dbd-text">T</p>
         <p className="day-number">9</p>
         <img src="../src/assets/images/Blowing-snow.png" alt="snow" />
+        <p className="dbd-temperature">14°</p>
         <div className="small-rectangle" />
+        <p className="dbd-temperature">8°</p>
       </div>
       <div className="rectangle">
         <p className="dbd-text">F</p>
         <p className="day-number">10</p>
         <img src="../src/assets/images/Blowing-snow.png" alt="snow" />
+        <p className="dbd-temperature">11°</p>
         <div className="small-rectangle" />
+        <p className="dbd-temperature">5°</p>
       </div>
     </section>
   );
