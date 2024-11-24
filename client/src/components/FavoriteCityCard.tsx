452905886651fe24ed4cf957abe1575e8e0f7cb6
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-24 16:14:16 +0100

    finally change handleClickVisibility paraneter by data cityName of the localStorage + handleClickGarbage for remove city in cityCard page and in the localStorage

--- client/src/components/FavoriteCityCard.tsx
+++ client/src/components/FavoriteCityCard.tsx
@@ -1,28 +1,27 @@
 import "/src/style/FavoriteCityCard.css";
-import { useRef } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import rubbishBin from "../assets/icons/rubbish-bin.png";
 import visibilityIcon from "../assets/icons/visibility-icon.png";
 import type CityOutletContextType from "../types/Outletcontext";
 
-export default function FavoriteCityCard() {
-  const h2Ref = useRef<HTMLHeadingElement>(null);
+interface FavoriteCityCardProps {
+  name: string;
+  id: string;
+  onClickGarbage: (id: string) => void;
+}
 
+export default function FavoriteCityCard({
+  name,
+  id,
+  onClickGarbage,
+}: FavoriteCityCardProps) {
   const outletContext = useOutletContext<CityOutletContextType>();
   const navigate = useNavigate();
 
   const handleClickVisibility = () => {
-    if (h2Ref?.current) {
-      const cityName = h2Ref.current.textContent as string;
-      outletContext.setCity(cityName);
-    }
-
+    outletContext.setCity(name);
     navigate("/today");
   };
-
-  // const handleClickRemove = (
-
-  // )
 
   // TODO: function for remove city of localStorage
   //TODO: map on cities in localStorage
@@ -30,9 +29,7 @@ export default function FavoriteCityCard() {
   return (
     <section className="city-favorite-card">
       <div className="city-favorite-card-header">
-        <h2 className="city-favorite-card-title" ref={h2Ref}>
-          Nantes
-        </h2>
+        <h2 className="city-favorite-card-title">{name}</h2>
       </div>
       <div className="city-favorite-card-footer">
         <div className="visibility-container">
@@ -41,8 +38,7 @@ export default function FavoriteCityCard() {
           </button>
         </div>
         <div className="rubbish-bin-container">
-          <button type="button">
-            {/* onClick={handleClickRemove} */}
+          <button type="button" onClick={() => onClickGarbage(id)}>
             <img className="rubbish-bin-icon" src={rubbishBin} alt="" />
           </button>
         </div>
