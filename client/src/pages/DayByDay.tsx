da7663b47e87b3b7bada91936c4f799447c7246e
Julien-44100 <julienbotar44100@yahoo.com>
2024-11-27 09:35:31 +0100

    finalisation de l'API dans DayByDay.tsx manque plus que le css

--- client/src/pages/DayByDay.tsx
+++ client/src/pages/DayByDay.tsx
@@ -1,105 +1,160 @@
 import "/src/style/DayByDay.css";
-import "/src/assets/images/Cloudy.png/";
-// import { useEffect, useState } from "react";
+
+import { useEffect, useState } from "react";
 
 function DayByday() {
   //  https://codesandbox.io/p/sandbox/ftn8jn LIEN SANDBOX A NE PAS PERDE !!!!!!!!
 
+  // TODO : clean code with a object for exemple
   // const [moredata, setData] = useState("");
-  // const [skyState, setSkyState] = useState("");
-  // const [temperaturemin, setTemperatureMin] = useState<number>();
-  // const [temperature, setTemperature] = useState<number>();
-  // const [realFeel, setRealFeel] = useState<number>();
-  // useEffect(() => {
-  //   fetch(
-  //     "https://api.openweathermap.org/data/2.5/forecast?q=Nantes&cnt1=1&appid={import.meta.env.VITE_OPENWEATHER_API_KEY}"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       setSkyState(data.list[8].weather[0].icon);
-  //       setTemperatureMin(Math.round(data.list[8].main.temp_min));
-  //       setTemperature(Math.round(data.list[8].main.temp_max));
-  //       setRealFeel(Math.round(data.list[8].main.feels_like));
-  //       console.log(moredata);
-  //       console.log(data.list[8].weather[0].icon);
-  //       console.log(data.list[8].main.temp_min);
-  //       console.log(data.list[8].main.temp_max);
-  //       console.log(data.list[8].main.feels_like);
-  //     });
-  // }, []);
+  const [skyState, setSkyState] = useState("");
+  const [skyState1, setSkyState1] = useState("");
+  const [skyState2, setSkyState2] = useState("");
+  const [skyState3, setSkyState3] = useState("");
+  const [skyState4, setSkyState4] = useState("");
+  const [temperaturemin, setTemperatureMin] = useState<number>();
+  const [temperaturemin1, setTemperatureMin1] = useState<number>();
+  const [temperaturemin2, setTemperatureMin2] = useState<number>();
+  const [temperaturemin3, setTemperatureMin3] = useState<number>();
+  const [temperaturemin4, setTemperatureMin4] = useState<number>();
+  const [temperature, setTemperature] = useState<number>();
+  const [temperature1, setTemperature1] = useState<number>();
+  const [temperature2, setTemperature2] = useState<number>();
+  const [temperature3, setTemperature3] = useState<number>();
+  const [temperature4, setTemperature4] = useState<number>();
+  const [skyDate, setSkyDate] = useState("");
+  const [skyDate1, setSkyDate1] = useState("");
+  const [skyDate2, setSkyDate2] = useState("");
+  const [skyDate3, setSkyDate3] = useState("");
+  const [skyDate4, setSkyDate4] = useState("");
 
-  // return (
-  //   <div className="App">
-  //     <figcaption>
-  //       <img
-  //         src={`http://openweathermap.org/img/wn/${skyState}@2x.png`}
-  //         alt="symbole de l'état du ciel"
-  //       />
-  //     </figcaption>
-  //     <div className="temperaturemin">{temperaturemin}°</div>
-  //     <div className="temperature">{temperature}°</div>
-  //     <div className="real-feel">{realFeel}°</div>
-  //   </div>
-  // );
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=Nantes&cnt1=1&appid=db33a6aa09eb95eb5bae868283feca9e&units=metric",
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // setData(data);
+        setSkyState(data.list[0].weather[0].icon);
+        setSkyState1(data.list[16].weather[0].icon);
+        setSkyState2(data.list[24].weather[0].icon);
+        setSkyState3(data.list[32].weather[0].icon);
+        setSkyState4(data.list[39].weather[0].icon);
+        setTemperature(Math.round(data.list[8].main.temp_max));
+        setTemperature1(Math.round(data.list[16].main.temp_max));
+        setTemperature2(Math.round(data.list[24].main.temp_max));
+        setTemperature3(Math.round(data.list[32].main.temp_max));
+        setTemperature4(Math.round(data.list[32].main.temp_max));
+        setTemperatureMin(Math.round(data.list[8].main.temp_min));
+        setTemperatureMin1(Math.round(data.list[16].main.temp_min));
+        setTemperatureMin2(Math.round(data.list[24].main.temp_min));
+        setTemperatureMin3(Math.round(data.list[32].main.temp_min));
+        setTemperatureMin4(Math.round(data.list[39].main.temp_min));
+        setSkyDate(data.list[8].dt_txt);
+        setSkyDate1(data.list[16].dt_txt);
+        setSkyDate2(data.list[24].dt_txt);
+        setSkyDate3(data.list[32].dt_txt);
+        setSkyDate4(data.list[39].dt_txt);
+        // console.log(moredata);
+        // console.log(data.list[0].weather[0].icon);
+        // console.log(data.list[0].main.temp_min);
+        // console.log(data.list[0].main.temp_max);
+      });
+  }, []);
 
-  // // db33a6aa09eb95eb5bae868283feca9e
+  function getDayName(dateStr: string, locale: string) {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString(locale, { weekday: "long" });
+  }
+  getDayName(skyDate, "en-US");
+  const dayname = getDayName(skyDate, "en-US");
+  const firstlettre = Array.from(dayname)[0];
+  const secondnumber = Array.from(skyDate)[8] + Array.from(skyDate)[9];
+  const dayname1 = getDayName(skyDate1, "en-US");
+  const firstlettre1 = Array.from(dayname1)[0];
+  const secondnumber1 = Array.from(skyDate1)[8] + Array.from(skyDate1)[9];
+  const dayname2 = getDayName(skyDate2, "en-US");
+  const firstlettre2 = Array.from(dayname2)[0];
+  const secondnumber2 = Array.from(skyDate2)[8] + Array.from(skyDate2)[9];
+  const dayname3 = getDayName(skyDate3, "en-US");
+  const firstlettre3 = Array.from(dayname3)[0];
+  const secondnumber3 = Array.from(skyDate3)[8] + Array.from(skyDate3)[9];
+  const dayname4 = getDayName(skyDate4, "en-US");
+  const firstlettre4 = Array.from(dayname4)[0];
+  const secondnumber4 = Array.from(skyDate4)[8] + Array.from(skyDate4)[9];
 
   return (
     <section className="day">
       <div className="container-rectangle">
         <div className="rectangle">
-          <p className="dbd-text">M</p>
-          <p className="day-number">6</p>
-          <img src="../src/assets/images/Cloudy.png" alt="cloud" />
-          <p className="dbd-temperature">17°</p>
+          <div className="date">{firstlettre}</div>
+          <div className="date">{secondnumber}</div>
+          <figcaption>
+            <img
+              src={`http://openweathermap.org/img/wn/${skyState}@2x.png`}
+              alt="symbole de l'état du ciel"
+            />
+          </figcaption>
+          <div className="temperature">{temperature}°</div>
           <div className="small-rectangle" />
-          <p className="dbd-temperature">9°</p>
+          <div className="temperaturemin">{temperaturemin}°</div>
         </div>
       </div>
       <div className="rectangle">
-        <p className="dbd-text">T</p>
-        <p className="day-number">7</p>
-        <img src="../src/assets/images/Cloudy.png" alt="cloud" />
-        <p className="dbd-temperature">18°</p>
+        <div className="date">{firstlettre1}</div>
+        <div className="date">{secondnumber1}</div>
+        <figcaption>
+          <img
+            src={`http://openweathermap.org/img/wn/${skyState1}@2x.png`}
+            alt="symbole de l'état du ciel"
+          />
+        </figcaption>
+        <div className="temperature">{temperature1}°</div>
         <div className="small-rectangle" />
-        <p className="dbd-temperature">9°</p>
+        <div className="temperaturemin">{temperaturemin1}°</div>
       </div>
 
       <div className="rectangle">
-        <p className="dbd-text">W</p>
-        <p className="day-number">8</p>
-        <img src="../src/assets/images/raining.png" alt="rain" />
-        <p className="dbd-temperature">15°</p>
+        <div className="date">{firstlettre2}</div>
+        <div className="date">{secondnumber2}</div>
+        <figcaption>
+          <img
+            src={`http://openweathermap.org/img/wn/${skyState2}@2x.png`}
+            alt="symbole de l'état du ciel"
+          />
+        </figcaption>
+        <div className="temperature">{temperature2}°</div>
         <div className="small-rectangle" />
-        <p className="dbd-temperature">7°</p>
+        <div className="temperaturemin">{temperaturemin2}°</div>
       </div>
       <div className="rectangle">
-        <p className="dbd-text">T</p>
-        <p className="day-number">9</p>
-        <img
-          className="day-friday"
-          src="../src/assets/images/Blowing-snow.png"
-          alt="snow"
-        />
-        <p className="dbd-temperature">14°</p>
+        <div className="date">{firstlettre3}</div>
+        <div className="date">{secondnumber3}</div>
+        <figcaption>
+          <img
+            src={`http://openweathermap.org/img/wn/${skyState3}@2x.png`}
+            alt="symbole de l'état du ciel"
+          />
+        </figcaption>
+        <div className="temperature">{temperature3}°</div>
         <div className="small-rectangle" />
-        <p className="dbd-temperature">8°</p>
+        <div className="temperaturemin">{temperaturemin3}°</div>
       </div>
       <div className="rectangle">
-        <p className="dbd-text">F</p>
-        <p className="day-number">10</p>
-        <img
-          className="day-friday"
-          src="../src/assets/images/Blowing-snow.png"
-          alt="snow"
-        />
-        <p className="dbd-temperature">11°</p>
+        <div className="date">{firstlettre4}</div>
+        <div className="date">{secondnumber4}</div>
+
+        <figcaption>
+          <img
+            src={`http://openweathermap.org/img/wn/${skyState4}@2x.png`}
+            alt="symbole de l'état du ciel"
+          />
+        </figcaption>
+        <div className="temperature">{temperature4}°</div>
         <div className="small-rectangle" />
-        <p className="dbd-temperature">5°</p>
+        <div className="temperaturemin">{temperaturemin4}°</div>
       </div>
     </section>
   );
 }
-
 export default DayByday;
