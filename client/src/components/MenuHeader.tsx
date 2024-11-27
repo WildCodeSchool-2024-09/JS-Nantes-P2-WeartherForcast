22a380b26ee826b819eebb99423e2f8d906d2f7a
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-11-27 11:48:48 +0100

    fix closed menu-header when usr click on nav-btn

--- client/src/components/MenuHeader.tsx
+++ client/src/components/MenuHeader.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { geolocaliseMe } from "../utilitiesFunctions/geolocation";
 
 export default function MenuHeader(props: {
@@ -7,26 +7,46 @@ export default function MenuHeader(props: {
     setBackground: React.Dispatch<React.SetStateAction<string>>;
     setColorCircle: (value: string) => void;
     setIdCity: (id: string) => void;
+    setMenuOpen: (value: boolean) => void;
   };
 }) {
-  const { setCity, setBackground, setColorCircle, setIdCity } = props.setters;
+  const { setCity, setBackground, setColorCircle, setIdCity, setMenuOpen } =
+    props.setters;
   const navigate = useNavigate();
   return (
     <section className="menu-dropdown">
       <nav>
         <button
-          className="geoloc-button"
+          className="nav-button"
           type="button"
           onClick={() => {
             geolocaliseMe(setCity, setBackground, setColorCircle, setIdCity);
             navigate("/today"); //👉FIXE ME LATER? WARNING geolocaliseMe is not async so maybe a problem with the promise API who could arrive after the navigation
+            setMenuOpen(false);
           }}
         >
           Geolocalise me
         </button>
-        <NavLink to="/myCities">My cities</NavLink>
-        <NavLink to="/preferenciesWear">My preferencies</NavLink>
-        {/* 💡 <NavLink/> instead of <Link> allow to give an automatically active className for each link if you do a class named .active DOC=> https://reactrouter.com/en/main/components/nav-link*/}
+        <button
+          className="nav-button"
+          type="button"
+          onClick={() => {
+            navigate("/myCities");
+            setMenuOpen(false);
+          }}
+        >
+          My cities
+        </button>
+        <button
+          className="nav-button"
+          type="button"
+          onClick={() => {
+            navigate("/preferenciesWear");
+            setMenuOpen(false);
+          }}
+        >
+          My preferencies
+        </button>
       </nav>
     </section>
   );
